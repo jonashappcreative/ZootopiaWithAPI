4d1b0a0b74127ed72a8cc03927839e500ae11585
Jonas Happ <github@jonashapp.com>
2024-05-04 22:43:07 +0200

    Added data fetcher functionality, returns list of animals of search word.

---
+++ data_fetcher.py
@@ -0,0 +1,35 @@
+import requests
+
+API_KEY = "wbvM4JFhbZZA1fhJnftgdv6F59dpO1sFkqBkvn9d"
+
+user_request = "Dog"
+
+
+def fetch_data(user_request):
+    """
+    Fetches the animals data for the animal 'user_request'.
+    Returns: a list of animals, each animal is a dictionary:
+    {
+      'name': ...,
+      'taxonomy': {
+        ...
+      },
+      'locations': [
+        ...
+      ],
+      'characteristics': {
+        ...
+      }
+    },
+    """
+    api_url = 'https://api.api-ninjas.com/v1/animals?name={}'.format(user_request)
+    response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
+    if response.status_code == requests.codes.ok:
+        # print(response.json())
+        return response.json()
+    else:
+        print("Error:", response.status_code, response.text)
+
+
+data = fetch_data(user_request)
+print(data)
